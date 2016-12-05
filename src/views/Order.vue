9e67b874f94f3d0a18895b6c7e579fea88682803
任刚在 <tank2xxl@gmail.com>
2016-12-05 14:53:20 +0800

    add Order.vue

---
+++ src/views/Order.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        Order.vue
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
